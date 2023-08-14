a7bda42ecff28290895a208cadafca55ce061051
We Are Anonymous <pengibot@gmail.com>
2023-08-14 09:22:21 +0100

    Got a second graph completed. Going to stick there and move on to Correlation window or I will run out of time!

--- gui/windows/GraphWindow.py
+++ gui/windows/GraphWindow.py
@@ -19,7 +19,6 @@ class GraphWindow:
         self.graph_data_items = self.data_manager.extract_graph_data()
         LoggerFactory.get_logger().info("Initialized Graph Window")
         self.window = tk.Toplevel(master)
-        # self.window.grab_set()
         self.window.title("Graph")
         icon = tk.PhotoImage(file="gui/assets/graph.png")
         self.window.iconphoto(False, icon)
@@ -32,60 +31,49 @@ class GraphWindow:
 
     def plot_graph(self):
 
-        data = []
-
-        for graph_data_item in  self.graph_data_items:
-            data.append([graph_data_item.eid, graph_data_item.site])
-
-        columns = ["EID", "Site"]
-
-        df = pd.DataFrame(data=data, columns=columns)
-
         # Real code below here
 
-        fig, ax = plt.subplots(figsize=(10, 10), nrows=2, ncols=2)
+        fig, ax = plt.subplots(figsize=(10, 6), nrows=1, ncols=2)
+
+        # Top Left
+
+        # Code for Top Left Site Distribution per EID
+        data = []
+        for graph_data_item in self.graph_data_items:
+            data.append([graph_data_item.eid, graph_data_item.site])
+        columns = ["EID", "Site"]
+        df = pd.DataFrame(data=data, columns=columns)
 
         value_counts = df["EID"].value_counts()
         unique_values = value_counts.index.tolist()
-        # Top Left
-        ax[0, 0].barh(unique_values, value_counts)
-        ax[0, 0].spines["right"].set_visible(False)
-        ax[0, 0].spines["top"].set_visible(False)
-        ax[0, 0].set_xlabel("Number of Sites", fontsize=8)
-        ax[0, 0].set_ylabel("EID", fontsize=8)
+        ax[0].barh(unique_values, value_counts)
+        ax[0].spines["right"].set_visible(False)
+        ax[0].spines["top"].set_visible(False)
+        ax[0].set_xlabel("Number of Sites", fontsize=8)
+        ax[0].set_ylabel("EID", fontsize=8)
         # Setting x-axis to display only whole numbers
-        ax[0, 0].xaxis.set_major_locator(MultipleLocator(1))
-        ax[0, 0].set_title("Site Distribution per EID")
+        ax[0].xaxis.set_major_locator(MultipleLocator(1))
+        ax[0].set_title("Site Distribution per EID")
 
         # Top Right
-        ax[0, 1].barh(unique_values, value_counts)
-        ax[0, 1].spines["right"].set_visible(False)
-        ax[0, 1].spines["top"].set_visible(False)
-        ax[0, 1].set_xlabel("Number of Sites", fontsize=8)
-        ax[0, 1].set_ylabel("EID", fontsize=8)
-        # Setting x-axis to display only whole numbers
-        ax[0, 1].xaxis.set_major_locator(MultipleLocator(1))
-        ax[0, 1].set_title("Graph 2")
 
-        # Bottom Left
-        ax[1, 0].barh(unique_values, value_counts)
-        ax[1, 0].spines["right"].set_visible(False)
-        ax[1, 0].spines["top"].set_visible(False)
-        ax[1, 0].set_xlabel("Number of Sites", fontsize=8)
-        ax[1, 0].set_ylabel("EID", fontsize=8)
-        # Setting x-axis to display only whole numbers
-        ax[1, 0].xaxis.set_major_locator(MultipleLocator(1))
-        ax[1, 0].set_title("Graph 3")
+        # Code for Top Left Site Distribution per EID
+        data = []
+        for graph_data_item in self.graph_data_items:
+            data.append([graph_data_item.site, graph_data_item.freq])
+        columns = ["Site", "Freq."]
+        df = pd.DataFrame(data=data, columns=columns)
 
-        # Bottom Right
-        ax[1, 1].barh(unique_values, value_counts)
-        ax[1, 1].spines["right"].set_visible(False)
-        ax[1, 1].spines["top"].set_visible(False)
-        ax[1, 1].set_xlabel("Number of Sites", fontsize=8)
-        ax[1, 1].set_ylabel("EID", fontsize=8)
-        # Setting x-axis to display only whole numbers
-        ax[1, 1].xaxis.set_major_locator(MultipleLocator(1))
-        ax[1, 1].set_title("Graph 4")
+        ax[1].bar(df["Site"], df["Freq."])
+        ax[1].spines["right"].set_visible(False)
+        ax[1].spines["top"].set_visible(False)
+        ax[1].set_xlabel("Site", fontsize=8)
+        ax[1].set_ylabel("Freq.", fontsize=8)
+        ax[1].set_xticklabels(df["Site"], rotation=90)
+        ax[1].set_title("Freq. at each Site")
+
+        plt.tight_layout()
+
 
         return fig
 
