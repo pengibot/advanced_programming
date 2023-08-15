5e8f730795abce434cc76d80091fba587d9823e9
We Are Anonymous <pengibot@gmail.com>
2023-08-15 15:46:25 +0100

    Added a lambda to ensure window closes properly on exit

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import tkinter as tk
 
 from gui.MainWindow import MainWindow
-from utils import LoggerFactory
+
 
 def main():
     """
         The execution point for the program file
     """
 
-    LoggerFactory.get_logger().info("Starting Application")
     window = tk.Tk()  # Display the root window and manage all other components
     window.configure(bg='white')
     window.title("Advanced Programming")
@@ -16,9 +15,9 @@ def main():
     window.resizable(False, False)  # Preventing windows from being resized
     photo = tk.PhotoImage(file="gui/assets/logo.png")
     window.iconphoto(False, photo)  # Adding icon for window
+    window.protocol("WM_DELETE_WINDOW", lambda: [window.destroy(), window.quit()])  # Call functions on close
     MainWindow(window)
     window.mainloop()
-    LoggerFactory.get_logger().info("Exiting Application")
 
 
 # Execute code when the file is run as a script, but not when it's imported
